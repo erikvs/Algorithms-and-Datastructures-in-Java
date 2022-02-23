02d34a7e2fa03472b99eaddd82e23789452b92ae
erikvs <erikvs@hiof.no>
2022-02-23 17:56:46 +0100

    Completed program. Takes user input for board size and starting position.

--- Oblig2/src/Board.java
+++ Oblig2/src/Board.java
@@ -1,33 +1,27 @@
-import java.util.Random;
-
 public class Board {
 
-    int used = 0, free = 1, size, Board[][], counter;
-
+    int free = 0, size, Board[][];
 
     //constructor for board
-    public Board(int size) {
+    public Board(int size, int x, int y) {
     //defines a fresh board of size by size and defines each space as free
     this.size = size;
     Board = new int[size][size];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++)
-                    Board[x][y] = free;
-
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++)
+                    Board[i][j] = free;
         }
-      //  System.out.println(Board);
+        //Mark defined starting space as "first move"
+        Board[x][y] = 1;
     }
 
     //method for finding a path - input param is starting space
     boolean solveBoard(int counter, int x, int y) {
 
         //Stop criterion is having moved all the spaces
-        if (counter == size * size + 1) { //TODO: understand why is this +1
+        if (counter == size * size +1) {
             return true;
         }
-
-        //Mark space as used
-        Board[x][y] = used;
 
         //All possible moves
         int moveX[] = {2, 1, -1, -2, -2, -1, 1, 2};
@@ -38,31 +32,20 @@ public class Board {
             int newY = y + moveY[k];
 
             //Checking if it is legal to move to new position
-            if (newX >= 0 && newX < size && newY >=0 && newY < size && Board[newX][newY] == free) {
-                Board[newX][newY] = counter; //this increments counter and decrements when backtracked
+            if (newX >= 0 && newX < size && newY >= 0 && newY < size && Board[newX][newY] == free) {
+                //increments counter and decrements it when backtracked
+                Board[newX][newY] = counter;
 
                 //try to find a new way forward recursively
-                if (solveBoard(counter +1, newX, newY)) {
-                    Board[newX][newY] = used;
+                if (solveBoard(counter+1, newX, newY)) {
+                    Board[newX][newY] = counter;
                     return true;
                 }
 
                 //Backtracking
                 Board[newX][newY] = free;
-
             }
-
         }
-        // I SPRINGERPROBLEMET må dette siste tilfellet, der vi ikke
-        // finner noen lovlig løsning med start i rute (i,j),
-        // behandles litt anderledes. I labyrinten er det ingen vits i
-        // å gå tilbake til en rute der vi har vært før. I
-        // springerproblemet er det ikke slik, der må vi nå markere at
-        // ruten er blitt ledig igjen, slik at den kan brukes på nytt
-        // i senere forsøk på å bygge ut en løsning. Øvrig
-        // datastruktur som brukes til å lagre løsningen må også
-        // oppdateres slik at dette steget som ikke ledet til løsning
-        // blir fjernet.
         return false;
     }
 
@@ -74,10 +57,10 @@ public class Board {
                 if (Board[i][j] == free)
                     result += "* ";
                 else
+                    //if a path is found, print a board which has tracked knights movement
                     result += Board[i][j] + " ";
             result += "\n";
         }
-
         return result;
     }
 }
--- Oblig2/src/Main.java
+++ Oblig2/src/Main.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-      //  Scanner userInput = new Scanner(System.in);
-     //   System.out.println("Generate a chessboard of n x n size where n the integer: ");
-       // int size = userInput.nextInt();
-        //TODO: start position input
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Generate a chessboard of n x n size where n is the integer: ");
+        int size = userInput.nextInt();
+        System.out.println("X coordinate for knight starting position: ");
+        int x = userInput.nextInt();
+        System.out.println("Y coordinate for knight starting position: ");
+        int y = userInput.nextInt();
 
         //Creating the board
-        Board board = new Board(5);
+        Board board = new Board(size, x, y);
 
         //Finding path
-        boolean solveBoard = board.solveBoard(0,2,3); //TODO: input param should be user input
+        boolean solveBoard = board.solveBoard(2, x, y);
 
         //Printing board and path (if any was found)
         System.out.println(board);
         if (!solveBoard){
             System.out.println("NO VIABLE PATH");
         }
-
     }
 }
