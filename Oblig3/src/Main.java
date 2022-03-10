833de0d25ac521594e84af559d49c414e2ac3e43
erikvs <erikvs@hiof.no>
2022-03-10 19:46:21 +0100

    oblig 3 finished
    final

--- Oblig3/src/Main.java
+++ Oblig3/src/Main.java
@@ -1,120 +1,141 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        //TODO: quicksort only works for 21 000 or lower.
-        int n = 21000; //TODO: make this store user input
-        int[] toBeSorted = new int[n];
-        long time = 0;
+        while (true) {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Define how many numbers to generate as n:\n");
 
-        //fills array with n random ints
-        Randomize.randomize(toBeSorted);
+            int n = userInput.nextInt();
+            int[] toBeSorted = new int[n];
+            double time = 0;
+            int i;
+            double sumTimeComplexity = 0;
 
-        int sortMethod = 4; //TODO: make this store user input
+            System.out.println("Define which sort method to test or quit the program: ");
+            System.out.println("1 - Insertion sort");
+            System.out.println("2 - Quick sort");
+            System.out.println("3 - Merge sort");
+            System.out.println("4 - Radix sort");
+            System.out.println("5 - Quit\n ");
+            int sortMethod = userInput.nextInt();
 
-        switch (sortMethod) {
-            case 1:
+            switch (sortMethod) {
+                case 1:
+                    System.out.println("Perform sort (1) or calculate Average Time Complexity (2):\n");
+                    int insertionSortTest = userInput.nextInt();
 
-                int insertionSortTest = 2; //TODO: make this store user input
-
-                switch (insertionSortTest) {
-                    case 1:
-                        time = System.currentTimeMillis();
-                        Sorting.insertionSort(toBeSorted);
-                        time = System.currentTimeMillis() - time;
-                        System.out.printf("Runtime for Insertion Sort\t: %6.3f s\n", time / 1000.0);
-                        break;
-                    case 2:
-                        int i;
-                        double sumTimeComplexity = 0;
-                        for (i = 0; i < 15; i++) {
+                    switch (insertionSortTest) {
+                        case 1:
+                            Randomize.randomize(toBeSorted);
                             time = System.currentTimeMillis();
                             Sorting.insertionSort(toBeSorted);
                             time = System.currentTimeMillis() - time;
+                            System.out.printf("Runtime for Insertion Sort\t: %6.3f seconds\n", time / 1000.0);
+                            System.out.println();
+                            break;
+                        case 2:
+                            for (i = 0; i < 15; i++) {
+                                Randomize.randomize(toBeSorted);
+                                time = System.currentTimeMillis();
+                                Sorting.insertionSort(toBeSorted);
+                                time = System.currentTimeMillis() - time;
 
-                            double c = time / Math.pow(n, 2); //TODO Think math here is fine, need to get that confirmed.
-                            sumTimeComplexity = sumTimeComplexity + c;
-                        }
-                        System.out.printf("Average Time Complexity for Insertion Sort\t: %6.10f \n", sumTimeComplexity / 15); //TODO: what is this value, milliseconds?
-                        break;
-                }
-                break;
+                                double c = time / Math.pow(n, 2);
+                                sumTimeComplexity = sumTimeComplexity + c;
+                            }
+                            System.out.printf("Average Time Complexity for Insertion Sort\t: %6.10f \n", sumTimeComplexity / 15);
+                            System.out.println();
+                            break;
+                    }
+                    break;
 
-            case 2:
-                int quickSortTest = 2; //TODO: make this store user input
-
-                switch (quickSortTest) {
-                    case 1:
-                        time = System.currentTimeMillis();
-                        Sorting.quickSort(toBeSorted, 0, toBeSorted.length - 1);
-                        time = System.currentTimeMillis() - time;
-                        System.out.printf("Runtime for Quick Sort\t: %6.3f s\n", time / 1000.0);
-                        break;
-                    case 2:
-                        int i;
-                        double sumTimeComplexity = 0;
-                        for (i = 0; i < 15; i++) {
+                case 2:
+                    System.out.println("Perform sort (1) or calculate Average Time Complexity (2):\n");
+                    int quickSortTest = userInput.nextInt();
+                    switch (quickSortTest) {
+                        case 1:
+                            Randomize.randomize(toBeSorted);
                             time = System.currentTimeMillis();
                             Sorting.quickSort(toBeSorted, 0, toBeSorted.length - 1);
                             time = System.currentTimeMillis() - time;
+                            System.out.printf("Runtime for Quick Sort\t: %6.3f seconds\n", time / 1000.0);
+                            System.out.println();
+                            break;
+                        case 2:
+                            for (i = 0; i < 15; i++) {
+                                Randomize.randomize(toBeSorted);
+                                time = System.currentTimeMillis();
+                                Sorting.quickSort(toBeSorted, 0, toBeSorted.length - 1);
+                                time = System.currentTimeMillis() - time;
+                                double c = time / (n * Math.log(n));
+                                sumTimeComplexity = sumTimeComplexity + c;
+                            }
+                            System.out.printf("Average Time Complexity for Quick Sort\t: %6.6f \n", sumTimeComplexity / 15);
+                            System.out.println();
+                            break;
+                    }
+                    break;
 
-                            double c = time / (n * Math.log(n));
-                            sumTimeComplexity = sumTimeComplexity + c;
-                        }
-                        System.out.printf("Average Time Complexity for Quick Sort\t: %6.6f \n", sumTimeComplexity / 15); //TODO: what is this value, milliseconds?
-                        break;
-                }
-                break;
-
-            case 3:
-                int mergeSortTest = 2; //TODO: make this store user input
-                switch (mergeSortTest) {
-                    case 1:
-                        time = System.currentTimeMillis();
-                        Sorting.mergeSort(toBeSorted, 0, toBeSorted.length - 1);
-                        time = System.currentTimeMillis() - time;
-                        System.out.printf("Runtime for Merge Sort\t: %6.3f s\n", time / 1000.0);
-                        break;
-                    case 2:
-                        int i;
-                        double sumTimeComplexity = 0;
-                        for (i = 0; i < 15; i++) {
+                case 3:
+                    System.out.println("Perform sort (1) or calculate Average Time Complexity (2):\n");
+                    int mergeSortTest = userInput.nextInt();
+                    switch (mergeSortTest) {
+                        case 1:
+                            Randomize.randomize(toBeSorted);
                             time = System.currentTimeMillis();
                             Sorting.mergeSort(toBeSorted, 0, toBeSorted.length - 1);
                             time = System.currentTimeMillis() - time;
+                            System.out.printf("Runtime for Merge Sort\t: %6.3f seconds\n", time / 1000.0);
+                            System.out.println();
+                            break;
+                        case 2:
+                            for (i = 0; i < 15; i++) {
+                                Randomize.randomize(toBeSorted);
+                                time = System.currentTimeMillis();
+                                Sorting.mergeSort(toBeSorted, 0, toBeSorted.length - 1);
+                                time = System.currentTimeMillis() - time;
+                                double c = time / (n * Math.log(n));
+                                sumTimeComplexity = sumTimeComplexity + c;
+                            }
+                            System.out.printf("Average Time Complexity for Merge Sort\t: %6.6f \n", sumTimeComplexity / 15);
+                            System.out.println();
+                            break;
+                    }
+                    break;
 
-                            double c = time /  (n * Math.log(n));
-                            sumTimeComplexity = sumTimeComplexity + c;
-                        }
-                        System.out.printf("Average Time Complexity for Merge Sort\t: %6.6f \n", sumTimeComplexity / 15); //TODO: what is this value, milliseconds?
-                        break;
-                }
-                break;
-
-            case 4:
-                int radixSortTest = 2; //TODO: make this store user input
-                switch (radixSortTest) {
-                    case 1:
-                        time = System.currentTimeMillis();
-                        Sorting.radixSort(toBeSorted, 10); // 10 because 0-9 is 10 digits
-                        time = System.currentTimeMillis() - time;
-                        System.out.printf("Runtime for Radix Sort\t: %6.3f s\n", time / 1000.0);
-                        break;
-
-                    case 2:
-                        int i;
-                        double sumTimeComplexity = 0;
-                        for (i = 0; i < 15; i++) {
+                case 4:
+                    System.out.println("Perform sort (1) or calculate Average Time Complexity (2):\n");
+                    int radixSortTest = userInput.nextInt();
+                    switch (radixSortTest) {
+                        case 1:
+                            Randomize.randomize(toBeSorted);
                             time = System.currentTimeMillis();
-                            Sorting.radixSort(toBeSorted, 10);
+                            Sorting.radixSort(toBeSorted, 10); // 10 because 0-9 is 10 digits
                             time = System.currentTimeMillis() - time;
-                            //TODO: casting to double correct? or redundant?
-                            double c = time / (double) (n * 10); //10 because 0-9 is 10 digits
-                            sumTimeComplexity = sumTimeComplexity + c;
-                        }
-                        System.out.printf("Average Time Complexity for Radix Sort\t: %6.10f \n", sumTimeComplexity / 15); //TODO: what is this value, milliseconds?
-                        break;
-                }
-                break;
+                            System.out.printf("Runtime for Radix Sort\t: %6.3f seconds\n", time / 1000.0);
+                            System.out.println();
+                            break;
+
+                        case 2:
+                            for (i = 0; i < 15; i++) {
+                                Randomize.randomize(toBeSorted);
+                                time = System.currentTimeMillis();
+                                Sorting.radixSort(toBeSorted, 10);
+                                time = System.currentTimeMillis() - time;
+                                double c = time / (n * 10); //10 because 0-9 is 10 digits
+                                sumTimeComplexity = sumTimeComplexity + c;
+                            }
+                            System.out.printf("Average Time Complexity for Radix Sort\t: %6.10f \n", sumTimeComplexity / 15);
+                            System.out.println();
+                            break;
+                    }
+                    break;
+
+                case 5:
+                    System.exit(1);
+            }
         }
     }
 }
